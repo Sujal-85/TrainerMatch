49f9fdfe512a2e3c79fa21c661cc0fa20a6d0859
SK8550 <khedekarsujay5@gmail.com>
2025-12-27 21:03:58 +0530

    I have completed profile pic functionality.

--- backend/src/modules/colleges/colleges.module.ts
+++ backend/src/modules/colleges/colleges.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from '../../prisma/prisma.module';
+import { NotificationsModule } from '../notifications/notifications.module';
 import { CollegesController } from './colleges.controller';
 import { CollegesService } from './colleges.service';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, NotificationsModule],
   controllers: [CollegesController],
   providers: [CollegesService],
   exports: [CollegesService],
